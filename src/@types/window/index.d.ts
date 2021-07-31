63cd80aaf00ce20478bb1eeb345ed421dfbac1f0
Efraa <efra23peralta@gmail.com>
2021-07-30 21:46:07 -0400

    feat: add microphone deps

--- src/@types/window/index.d.ts
+++ src/@types/window/index.d.ts
@@ -1,8 +1,15 @@
+import type { TGetUserMedia } from '../../providers/speech'
 import type { EventName } from '../../services/event-bus'
 
 declare global {
   interface Window {
     __PF_ASSISTANT_LOADED__?: boolean
+    webkitAudioContext: typeof AudioContext
+  }
+
+  interface Navigator {
+    webkitGetUserMedia?: TGetUserMedia
+    mozGetUserMedia?: TGetUserMedia
   }
 
   interface HTMLElementEventMap extends Record<EventName, CustomEvent> {}
